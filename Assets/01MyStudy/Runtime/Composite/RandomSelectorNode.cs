dec2893c89ffd30964e95a92cee21c33573581ad
peng984251536 <984251536@qq.com>
2024-10-14 14:26:32 +0800

    Game Framework插件

---
+++ Assets/01MyStudy/Runtime/Composite/RandomSelectorNode.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace MyEditorView.Runtime
+{
+    public class RandomSelectorNode : CompositeNode
+    {
+        protected override void OnReset()
+        {
+            
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        protected override State OnUpdate()
+        {
+            if (children.Count > 0)
+            {
+                int index = Random.Range(0, children.Count - 1);
+                var child = children[index];
+                if (child.Enable)
+                    return child.UpdateState();
+            }
+            return State.Success;
+        }
+        
+    }
+}
---
+++ Assets/01MyStudy/Runtime/Decorator/WaitNode.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace MyEditorView.Runtime
+{
+    public class WaitNode : DecoratorNode
+    {
+        public float duration = 1;
+
+        float startTime = 2f;
+
+        protected override void DoAction()
+        {
+            Owner.AddDeleteTime(startTime);
+        }
+        protected override State OnUpdate()
+        {
+            if (Time.time - startTime > duration)
+            {
+                return child.Enable ? child.UpdateState() : State.Success;
+            }
+            else
+                return State.Running;
+        }
+    }
+}
